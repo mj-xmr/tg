50206723081ca8da226d3887caf9e55e3f056f59
mj-xmr <mjxmr@protonmail.com>
2021-10-13 20:27:17 +0200

    Use ccache

--- util/ci-gcc.sh
+++ util/ci-gcc.sh
@@ -5,7 +5,13 @@ MAKE=make
 #BUILD_DIR="build"
 #mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
+# Use ccache
+export PATH=/usr/lib/ccache:$PATH
 export CFLAGS="-DTGL_AVOID_OPENSSL -w"
+
 autoconf
 ./configure && ${MAKE} -j`nproc` || ${MAKE}
+
+# Check if the binary runs at all
+bin/telegram-cli -h
 
