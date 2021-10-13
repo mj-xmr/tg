60c38a9f46610849fada4518121152241995a2f8
mj-xmr <mjxmr@protonmail.com>
2021-10-13 20:20:59 +0200

    Fix compilation

--- util/ci-gcc.sh
+++ util/ci-gcc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 MAKE=make
-GEN="CodeBlocks - Unix Makefiles"
-BUILD_DIR="build"
-
+#GEN="CodeBlocks - Unix Makefiles"
+#BUILD_DIR="build"
 #mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
-./configure && make -j`nproc` || make
+export CFLAGS="-DTGL_AVOID_OPENSSL -w"
+autoconf
+./configure && ${MAKE} -j`nproc` || ${MAKE}
 
